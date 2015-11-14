72ec2289f66b97c4855d0f1fbabdae99f2b04635
Luis Espada <lespadat@gmail.com>
2015-11-14 13:37:50 +0100

    sigo con las visuallizaciones de los cambios

--- maxEngines_withArgum.sh
+++ maxEngines_withArgum.sh
@@ -2,6 +2,6 @@
 
 # obtiene el nombre de la aerolinea con el avion con más motores
 
-echo la aerolinea es ...
+echo La compañía con el modelo con más motores es ...
 
 csvsort -d'^' -c nb_engines $1 -r | head -2 | csvcut -c manufacturer | tail -n +2 
